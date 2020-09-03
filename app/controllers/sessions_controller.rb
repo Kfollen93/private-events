4f219296d23ec9df0b76787ee34c7520cd6ea884
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-02 20:51:07 -0700

    Fixed signout path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,6 @@ class SessionsController < ApplicationController
     
     def destroy
         session.clear
-        redirect_to root_path
+        redirect_to root_path, :notice => 'You logged out successfully'
     end
 end
