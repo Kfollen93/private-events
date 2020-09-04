2d490e54116c3c2a70392bad08a31c797962092e
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-04 09:31:02 -0700

    Created and migrated event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_153659) do
+ActiveRecord::Schema.define(version: 2020_09_04_163019) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
