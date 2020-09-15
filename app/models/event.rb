9575149f01220a2cd4271e1879b8a239425e85cb
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-15 09:14:08 -0700

    Created attending model and migrated db

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
-    has_many :attendees, through: :attendances, source: :user
+    has_many :attendees, through: :attendings, source: :user
+    has_many :attendings
 end
