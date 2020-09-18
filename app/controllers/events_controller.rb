e2f752998ebd7e840b780b47aee8a2d51d44ed03
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-18 09:06:08 -0700

    Added validations for creating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,27 @@ class EventsController < ApplicationController
         end
     end
 
+    def edit
+        @event = Event.find(params[:id])
+    end
+
+    def update
+        @event = Event.find(params[:id])
+
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+
+        redirect_to current_user
+    end
+
     def show
         @event = Event.find(params[:id])
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 35 }
+    validates :body, presence: true, length: { maximum: 100 }
+    validate :future_event
+
     belongs_to :creator, class_name: "User"
     has_many :attendings
     has_many :attendees, through: :attendings, source: :user
+
+    def future_event
+        errors.add(:date, " Can't be in the past!") if date < Time.now
+    end
 end
