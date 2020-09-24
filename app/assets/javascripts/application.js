17ee07874204bdd9d15a1a25385ddd06e597a732
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-09-24 12:55:23 -0700

    Added javascript to burger navbar; Removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,28 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
 //= require_tree .
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    if ($navbarBurgers.length > 0) {
+  
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
